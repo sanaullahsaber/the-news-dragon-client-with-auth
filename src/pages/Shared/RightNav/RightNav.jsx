56f59264dd26f6107662576929e4c4f1d0d32441
sanaullahsabere <you@example.com>
2023-04-29 12:51:37 +0600

    first commit

---
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Button, ListGroup } from 'react-bootstrap';
+import { FaGoogle, FaGithub, FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa";
+import QZone from '../QZone/QZone';
+import bg from '../../../assets/bg.png'
+
+const RightNav = () => {
+  return (
+    <div>
+      <h4 className='mt-4'>Login With</h4>
+      <Button className="mb-2" variant="outline-primary">
+        <FaGoogle /> Login with Google
+      </Button>
+      <Button variant="outline-secondary">
+        <FaGithub /> Login with Github
+      </Button>
+      <div>
+        <h4 className='mt-4'>Find Us On</h4>
+        <ListGroup>
+          <ListGroup.Item><FaFacebookF></FaFacebookF>Facebook</ListGroup.Item>
+          <ListGroup.Item><FaTwitter></FaTwitter> Twitter</ListGroup.Item>
+          <ListGroup.Item><FaInstagram></FaInstagram>Instagram</ListGroup.Item>
+        </ListGroup>
+      </div>
+      <QZone></QZone>
+      <div>
+        <img src={bg} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default RightNav;
